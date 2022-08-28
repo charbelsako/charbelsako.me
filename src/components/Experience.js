2203f67d192dd70a1bf453eae939e84a9cc2b3c4
Sharbel Sarkis <charbelsako@gmail.com>
2022-08-28 01:42:48 +0000

    Solves linting errors

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,20 +1,11 @@
 import React, { useState } from "react"
 import Typography from "@material-ui/core/Typography"
 import Paper from "@material-ui/core/Paper"
-import { makeStyles } from "@material-ui/core/styles"
 import Container from "@material-ui/core/Container"
 import styles from "./styles"
 import Moment from "react-moment"
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: theme.spacing(3, 2),
-  },
-}))
-
 function Experience(props) {
-  const classes = useStyles()
-
   const [experience] = useState([
     {
       title: "Backend developer",
